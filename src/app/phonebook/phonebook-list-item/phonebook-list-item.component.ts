8db32c5ea9b1effab4f22fca0cc492c06591726b
Tomislav <>
2020-07-04 20:29:03 +0200

    Lato font added, item card style adjusted

--- src/app/phonebook/phonebook-list-item/phonebook-list-item.component.ts
+++ src/app/phonebook/phonebook-list-item/phonebook-list-item.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-phonebook-list-item',
-  templateUrl: './phonebook-list-item.component.html'
+  templateUrl: './phonebook-list-item.component.html',
+  styleUrls: ['./phonebook-list-item.component.scss']
 })
 export class PhonebookListItemComponent {
 }
--- src/app/phonebook/phonebook.component.ts
+++ src/app/phonebook/phonebook.component.ts
@@ -4,6 +4,7 @@ import {PhonebookService} from './service/phonebook.service';
 @Component({
   selector: 'app-phonebook',
   templateUrl: './phonebook.component.html',
+  styleUrls: ['./phonebook.component.scss'],
   providers: [PhonebookService]
 })
 export class PhonebookComponent {
